675a8c8952b1008d796440107c92987dc6c8f668
Melissa Pérez <84192851+MelissaPerez09@users.noreply.github.com>
2022-01-28 17:16:11 -0600

    comentarios

--- Radio.java
+++ Radio.java
@@ -1,10 +1,11 @@
 public interface Radio {
-  boolean encenderApagar();
-  String guardarEmisoraActual(int numBoton);
-  String seleccionarEmisoraGuardada(int numBoton);
-  String cambiarSenal(int opcion);
-  void subirEmisora();
-  void bajarEmisora();
-  float getEmisoraActual();
-  boolean comprobarEncendida();
+  public void encenderApagar(); //cambia el estado encendido/apagado
+  public String guardarEmisoraActual(int numBoton); //guarda la emisora como favorita en el botón indicado (array de 12 espacios).
+  public String seleccionarEmisoraGuardada(int numBoton); //reproduce la emisora guardada en uno de los 12 botones.
+  public String cambiarSenal(Boolean opcion); //true: AM //false: FM
+  public Boolean getTipoSenal(); //true: AM //false: FM
+  public void subirEmisora(); //aumenta la emisora actual en 0.2 si la señal actual es FM, o en 10 si es AM
+  public void bajarEmisora(); //disminuye la emisora actual en 0.2 si la señal actual es FM, o en 10 si es AM
+  public float getEmisoraActual(); //obtiene la emisora actual
+  public boolean comprobarEncendida(); //true: encendida, false: apagada
 }
